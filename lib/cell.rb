8d3d7b3650ddd0e7a2d8882d343b91d2f474e055
Jake Kim <imjakekim@gmail.com>
2022-07-12 14:53:08 -0700

    Finish iteration 1. hopefully

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,12 +1,49 @@
 require './lib/ship'
 class Cell
-  attr_reader :coordinate
+  attr_reader :coordinate, :ship
   def initialize(coordinate)
     @coordinate = coordinate
     @ship
+    @struck = false
   end
 
   def place_ship(ship)
     @ship = ship
   end
+
+  def empty?
+    @ship == nil
+  end
+
+  def fired_upon?
+    @struck
+  end
+
+  def fire_upon
+    if @ship != nil
+      @ship.hit
+    end
+
+    @struck = true
+  end
+
+  def render(option = false)
+
+    if !fired_upon? && option
+      "S"
+    elsif !fired_upon?
+      "."
+    elsif fired_upon? && empty?
+      "M"
+    elsif fired_upon? && @ship.sunk?
+      "X"
+    elsif fired_upon? && !empty?
+      "H"
+    end
+  end
+
+
+
+
+
 end
