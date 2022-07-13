90cdbd81880eac40b5291928a713f7ee51bc5541
Jake Kim <imjakekim@gmail.com>
2022-07-12 18:23:21 -0700

    Start board testing

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,3 +1,11 @@
 require "./lib/board"
 
-RSpec.describe Board
+RSpec.describe Board do
+  it 'Board class should exist' do
+    board = Board.new
+    expect(board).to be_an_instance_of(Board)
+    expect(board.cells).to be_an_instance_of(Hash)
+  end
+
+  
+end
