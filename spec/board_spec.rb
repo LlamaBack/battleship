2181f78dcb48014601114241f0ed05ddb2e060e3
Jake Kim <imjakekim@gmail.com>
2022-07-13 10:48:10 -0700

    Finish valid coordinates tests

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -7,5 +7,14 @@ RSpec.describe Board do
     expect(board.cells).to be_an_instance_of(Hash)
   end
 
-  
+  it 'Board class checks for valid coordinates' do
+    board = Board.new
+    expect(board.valid_coordinate?("A1")).to eq(true)
+    expect(board.valid_coordinate?("D4")).to eq(true)
+    expect(board.valid_coordinate?("A5")).to eq(false)
+    expect(board.valid_coordinate?("E1")).to eq(false)
+    expect(board.valid_coordinate?("A22")).to eq(false)
+  end
+
+
 end
