81c60e03015b34257795aed8de7ec325b7f983c4
Jake Kim <imjakekim@gmail.com>
2022-07-14 10:39:56 -0700

    Clean up valid coordinate tests

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -5,41 +5,38 @@ require './lib/cell'
 
 RSpec.describe Board do
   before :each do
-    @cell_id = Board.new()
+    @board = Board.new()
   end
 
   it 'class should exist' do
-    expect(@cell_id).to be_an_instance_of(Board)
+    expect(@board).to be_an_instance_of(Board)
   end
 
   it 'the cells should be link back to the Cell class'do
-    expect(@cell_id.cells['A1']).to be_an_instance_of Cell
+    expect(@board.cells['A1']).to be_an_instance_of Cell
   end #cells are coordinates on the board
 
   it 'should inform if a coordinate is on the board' do
-      board_cell = Board.new()
-    expect(board_cell.valid_coordinate?('A1')).to eq(true)
-      board_cell = Board.new()
-    expect(board_cell.valid_coordinate?('D4')).to eq(true)
-      board_cell = Board.new()
-    expect(board_cell.valid_coordinate?('A5')).to eq(false)
-      board_cell = Board.new()
-    expect(board_cell.valid_coordinate?('E1')).to eq(false)
-      board_cell = Board.new()
-    expect(board_cell.valid_coordinate?('A22')).to eq(false)
-      board_cell = Board.new()#false b/c the letter is not capitalized
-    expect(board_cell.valid_coordinate?('a1')).to eq(false) 
+    expect(board.valid_coordinate?('A1')).to eq(true)
+    expect(board.valid_coordinate?('D4')).to eq(true)
+    expect(board.valid_coordinate?('A5')).to eq(false)
+    expect(board.valid_coordinate?('E1')).to eq(false)
+    expect(board.valid_coordinate?('A22')).to eq(false)
+    expect(board.valid_coordinate?('a1')).to eq(false)
   end
 
-  # it 'should inform if a placement for a ship on the board is valid or not' do
-  # #check if boat & size completely fits within board
-  #     cruiser = Ship.new("Cruiser", 3)
-  #     submarine = Ship.new("Submarine", 2) 
+  xit 'should inform if a placement for a ship on the board is valid or not' do
+  #check if boat & size completely fits within board
+    cruiser = Ship.new("Cruiser", 3)
+    cruiser_place = ["A1", "A2", "A3"]
 
-  #   board_cell = Board.new('E1')
-  
-  #   expect().to eq()
-  # end
+    submarine = Ship.new("Submrine", 2)
+    submarine_place = ["B1", "C1"]
+
+    expect(@board.valid_placement(cruiser, cruiser_place)).to eq(true)
+    expect(@board.valid_placement(submarine, submarine_place)).to eq(true)
+
+  end
 
 
 end
