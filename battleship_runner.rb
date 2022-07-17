fc60fd5ba2e32891c38a0bc76719b7fcb2869090
Jake Kim <imjakekim@gmail.com>
2022-07-17 14:52:25 -0700

    starting iteration 3: main menu

---
+++ battleship_runner.rb
@@ -0,0 +1,24 @@
+require './lib/game'
+
+game = Game.new()
+
+
+
+
+
+#
+# p "Welcome to BATTLESHIP"
+# p "Enter p to play. Enter q to quit."
+# input = gets.chomp
+#
+#
+# if (input == 'p')
+#   game = GameStarter.new(input)
+#
+#   p "Welcome to BATTLESHIP"
+#   p "Enter p to play. Enter q to quit."
+#   input = gets.chomp
+#
+# elsif (input == 'q')
+#
+# end
---
+++ spec/game_spec.rb
@@ -0,0 +1,14 @@
+require './lib/game'
+
+RSpec.describe Game do
+  it 'Game exists' do
+    game = Game.new
+    expect(game).to be_an_instance_of(Game)
+  end
+
+  it 'Game has a welcome message' do
+    game = Game.new
+    expect(game).to output('Welcome to BATTLESHIP').to_stdout
+    expect(game).to output('Enter p to play. Enter q to quit.').to_stdout
+  end
+end
